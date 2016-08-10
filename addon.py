5b9f46edba5a5cc74985798adeb463e830015f46
daerhan1 <daerhan1@hotmail.com>
2016-08-10 21:13:54 +0800

    Update addon.py

--- addon.py
+++ addon.py
@@ -18,8 +18,13 @@ xbmcplugin.setContent(addon_handle, 'videos')
 
 names = [
 	'Mongol TV',
+	'NBC',
+	'ABC',
+	'CBS',
+	'NBC Golf',
+	'NBC Sports',
+	'NBA',
 	'Bloomberg TV',
-	'Bolobsural',
 	'MNB 1',
 	'MNB 2',
 	'UBS',
@@ -29,6 +34,7 @@ names = [
 	'TV5',
 	'TV9',
 	'25',
+	'EDU',
 	'Eagle',
 	'SBN',
 	'Royal',
@@ -36,8 +42,13 @@ names = [
 ]
 urls = [
 	'http://202.70.45.36/hls/_definst_/tv_mid/smil:mongolhd.smil/playlist.m3u8',
+	'http://cp.dmbshare.net:8000/live/aaron/aaron/4578.ts',
+	'http://cp.dmbshare.net:8000/live/aaron/aaron/4601.ts',
+	'http://cp.dmbshare.net:8000/live/aaron/aaron/4576.ts',
+	'http://cp.dmbshare.net:8000/live/aaron/aaron/4586.ts',
+	'http://cp.dmbshare.net:8000/live/aaron/aaron/4587.ts',
+	'http://cp.dmbshare.net:8000/live/aaron/aaron/4577.ts',
 	'http://202.70.45.36/hls/_definst_/tv_mid/smil:bloomberg.smil/playlist.m3u8',
-	'http://202.70.45.36/hls/_definst_/tv_mid/smil:edu.smil/playlist.m3u8',
 	'http://202.70.45.36/hls/_definst_/tv_mid/smil:mnb.smil/playlist.m3u8',
 	'http://202.70.45.36/hls/_definst_/tv_mid/smil:mnb_2.smil/playlist.m3u8',
 	'http://202.70.45.36/hls/_definst_/tv_mid/smil:ubs.smil/playlist.m3u8',
@@ -50,12 +61,17 @@ urls = [
 	'http://202.70.45.36/hls/_definst_/tv_mid/smil:edu.smil/playlist.m3u8',
 	'http://202.70.45.36/hls/_definst_/tv_mid/smil:eagle.smil/playlist.m3u8',
 	'http://202.70.45.36/hls/_definst_/tv_mid/smil:sbn.smil/playlist.m3u8',
-	'http://202.70.45.36/hls/_definst_/tv_mid/smil:royal.smil/playlist.m3u8'
+	'http://202.70.45.36/hls/_definst_/tv_mid/smil:royal.smil/playlist.m3u8',
+	'http://202.70.45.36/hls/_definst_/tv_mid/smil:parliament.smil/playlist.m3u8'
 ]
 icons=[
 	'mongol-tv.png',
+	'mongol-tv.png',
+	'mongol-tv.png',
+	'mongol-tv.png',
+	'mongol-tv.png',
+	'mongol-tv.png',
 	'bloomberg.png',
-	'edu_512x512.png',
 	'mnb_512x512.png',
 	'mn2_512x512.png',
 	'ubs_512x512.png',
@@ -64,10 +80,11 @@ icons=[
 	'ehoron_512x512.png',
 	'tv5_512x512.png',
 	'tv9_512x512.png',
-	'25.png',
+	'royalhd_512x512.png',
+	'edu_512x512.png',
 	'eagle_512x512.png',
 	'sbn_512x512.png',
-	'royalhd_512x512.png',	
+	'royalhd_512x512.png',
 	'royalhd_512x512.png',
 ]
 
